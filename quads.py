dfb23945c6de7864c6426a0f99cfae6ed2ba3469
Grady Williams <gradyrw@gmail.com>
2013-07-03 16:44:12 -0700

    Started clean up quads.py program. Everything still works.

--- quads.py
+++ quads.py
@@ -42,6 +42,10 @@ I = np.array([[I_x,0,0],[0,I_y,0],[0,0,I_z]])
 K_m = 1.5e-9
 K_f = 6.11e-8
 motor_gain = 1.0
+"""
+Define some paramaters for CUDA compilation
+"""
+block_dim = 64
 
 """
 PI^2 class for performing the PI^2 algorithm in the context of quadrotor navigation
@@ -53,64 +57,56 @@ class M_Pi_2:
     Initializes the PI^2 class, compiles all the CUDA code and stores them
     as callable functions
     """
-    def __init__(self, init_pos, forest, K, time, T):
-        self.init_state = np.require(init_pos, dtype=np.float32, requirements = ['A', 'O', 'W', 'C'])
-        self.init_state_d = gpuarray.to_gpu(init_state)
+    def __init__(self, init_state, forest, K, time, T):
+        #Store the initial stae values on both the CPU and GPU
+        self.state = init_state
+        self.state_d = self.on_gpu(state)
+        #K is the number of rollouts to be performed per trial.
         self.K = K
-        self.time = time
+        #T is the number of timesteps to be taken per rollout
         self.T = T
+        #dt is the amount of time take from one time-step to another.
         self.dt = time/(1.0*T)
+        #generator is a gpu function which quickly generates large amounts of normally
+        #distributed random numbers.
         self.generator = XORWOWRandomNumberGenerator()
+        #Initialize controls, state costs, and terminal costs as GPU arrays.
         self.controls_d = gpuarray.zeros(T*K*control_dimensions, dtype = np.float32)
         self.state_costs_d = gpuarray.zeros(T*K, dtype = np.float32)
         self.terminal_costs_d = gpuarray.zeros(K, dtype = np.float32)
-        self.forest = self.on_gpu(forest)
-        #Initialize CUDA functions
-        rollout_kernel, U_d, p_d = self.func1(T)
+        #Initialize the forest as a CPU and GPU array.
+        self.forest = forest
+        self.forest_d = self.on_gpu(forest)
+        #Compile and store CUDA functions
+        rollout_kernel, U_d = self.func1(T)
         cost_to_go = self.func2(T)
         reduction = self.func3()
         multiply = self.func4()
-        self.funcs = rollout_kernel, cost_to_go, reduction, multiply, U_d, p_d
+        self.funcs = rollout_kernel, cost_to_go, reduction, multiply, U_d
 
-    def rollouts(self, U, var1, var2, point, mode, test = False):
+    def rollouts(self, U, var1, var2, goal_state_d, test = False):
         #Get an array of random numbers from CUDA. The numbers are
-        #normally distributed with mean = 0 and variance = var
+        #standard normal, the variance is changed later on in the rollout_kernel
         du = self.generator.gen_normal(self.K*self.T*(control_dimensions), np.float32)
-        #Unpack CUDA functions from self.funcs, get addresses of array in constant 
-        #memory
-        rollout_kernel, cost_to_go, reduction, multiply, U_d, p_d = self.funcs
+        #Unpack CUDA functions from self.funcs, get addresses of current control
+        #array in constant memory
+        rollout_kernel, cost_to_go, reduction, multiply, U_d = self.funcs
         cuda.memcpy_dtod(U_d, U.ptr, U.nbytes)
-        cuda.memcpy_dtod(p_d, point.ptr, point.nbytes)
         #Set blocksize and gridsize for rollout and cost-to-go kernels
-        blocksize = (64, 1, 1)
-        gridsize = ((self.K - 1)//64 + 1, 1, 1)
-        #print "Rollout Kernel"
-        start = datetime.now()
-        mode = gpuarray.to_gpu(np.array(mode,dtype=np.int32))
+        blocksize = (block_dim, 1, 1)
+        gridsize = ((self.K - 1)//block_dim + 1, 1, 1)
         #Launch the kernel for simulating rollouts
         rollout_kernel(self.controls_d, self.state_costs_d, self.terminal_costs_d,
-                       du, self.init_state_d, self.forest, np.float32(self.dt), 
+                       du, self.state_d, self.forest_d, goal_state_d, np.float32(self.dt), 
                        np.float32(motor_gain),
                        np.float32(total_mass), np.float32(I_x), np.float32(I_y),
-                       np.float32(I_z), np.float32(l), np.float32(var1), np.float32(var2), mode,
+                       np.float32(I_z), np.float32(l), np.float32(var1), np.float32(var2),
                        grid=gridsize, block=blocksize)
         cuda.Context.synchronize()
         d = self.terminal_costs_d.get()
         d = np.mean(d)
         c = self.state_costs_d.get()
         c = np.mean(c)
-        """
-        print "State Costs"
-        print self.state_costs_d
-        print "Terminal Costs"
-        print self.terminal_costs_d
-        print "Controls"
-        print self.controls_d
-        print "Phi Angle"
-        print z_locs
-        print
-        print
-        """
         #Launch the kernel for computing cost-to-go values for each state
         blocksize = (self.T,1,1)
         gridsize = (self.K,1,1)
@@ -138,7 +134,6 @@ class M_Pi_2:
             cuda.Context.synchronize()
         normalizer = out_d
         cuda.Context.synchronize()
-        print point
         #Multipy the controls by the weighted score. The weighted score is the cost-to-go
         #function divided by the normalizer.
         start = datetime.now()
@@ -180,8 +175,8 @@ class M_Pi_2:
         #Defines the initial state of the quadrotors
         init_pos = np.zeros(16)
         init_pos[12:] = 3167.77
-        self.init_state = init_pos
-        self.init_state_d = self.on_gpu(init_pos)
+        self.state= init_pos
+        self.state_d = self.on_gpu(init_pos)
         #X,Y, and Z hold location data for plotting
         X = []
         Y = []
@@ -195,12 +190,12 @@ class M_Pi_2:
         count_max = 500
         stop = False
         while (count < count_max and not stop):
-            U,d,c = self.rollouts(U, 250, 0, p_d, 1)
+            U,d,c = self.rollouts(U, 250, 0, p_d)
             if (d < 20):
                 stop = True
             count += 1
         #Takeoff! Uses the pre-computed commands from the while loop
-        info = self.plotter(U.get().reshape(T,4), init_state, T, plot=False)
+        info = self.plotter(U.get().reshape(T,4), state, T, plot=False)
 
         #Prints the state of the quadrotors after the takeoff commands have executed.
         print "After takeoff algorithm"
@@ -208,8 +203,8 @@ class M_Pi_2:
         p_d = self.on_gpu(p)
         print info[0]
         #Records the new state into the class field
-        self.init_state = info[0]
-        self.init_state_d = self.on_gpu(info[0])
+        self.state= info[0]
+        self.state_d = self.on_gpu(info[0])
         #Records the takeoff trajectory
         X.extend(info[1])
         Y.extend(info[2])
@@ -219,14 +214,14 @@ class M_Pi_2:
         U = self.on_gpu(U)
         var = 500
         #Cancel Loop for now!!!!
-        while((abs(self.init_state[8]) > .05 or abs(2 - self.init_state[2]) > .05)):
-            U,d,c = self.rollouts(U, var, 0, p_d, 2)
+        while((abs(self.state[8]) > .05 or abs(2 - self.state[2]) > .05)):
+            U,d,c = self.rollouts(U, var, 0, p_d)
             U_old = U.get()
-            info = self.plotter(U_old.reshape(T,4), self.init_state, 1, plot=False)
-            self.init_state = info[0]
-            self.init_state_d = self.on_gpu(info[0])
+            info = self.plotter(U_old.reshape(T,4), self.state, 1, plot=False)
+            self.state= info[0]
+            self.state_d = self.on_gpu(info[0])
             #Prints some info about the curent state
-            print (self.init_state[2], self.init_state[8], self.init_state[12], np.mean(U.get()))
+            print (self.state[2], self.state[8], self.state[12], np.mean(U.get()))
             print
             X.extend(info[1])
             Y.extend(info[2])
@@ -241,7 +236,7 @@ class M_Pi_2:
         """
         while(count < 500):
             U,d,c = self.rollouts(U, 500, 1.5, p_d, 1)
-            print p_d, self.init_state_d
+            print p_d, self.state_d
             print
             print count
             print d,c
@@ -251,7 +246,7 @@ class M_Pi_2:
                 break
             count += 1
         print "======================================"
-        self.plotter(U.get().reshape(T,4), self.init_state,T,plot=True)
+        self.plotter(U.get().reshape(T,4), self.state,T,plot=True)
         """
         p = np.array([5,5,5], dtype=np.float32)
         p_d_test = self.on_gpu(p)
@@ -266,20 +261,20 @@ class M_Pi_2:
                 p = np.array([5,5,5])
                 p_d_test = self.on_gpu(p)
             print count
-            print "++++++++++++++++++++++++++++++++++++++++"
-            print p_d_test
-            U,d,c = self.rollouts(U, 500, 1.5, p_d_test, 2)
-            print p_d_test
-            print "========================================"
-            print p_d_test
-            print "ttttttttttttttttttttttttttttttttttttttttttt"
+            #print "++++++++++++++++++++++++++++++++++++++++"
+            #print p_d_test
+            U,d,c = self.rollouts(U, 500, 1.5, p_d_test)
+            #print p_d_test
+            #print "========================================"
+            #print p_d_test
+            #print "ttttttttttttttttttttttttttttttttttttttttttt"
             U_old = U.get()
-            info = self.plotter(U_old.reshape(T,4), self.init_state,1,trees = [3,3,7,1], plot=False)
-            print p_d_test
-            print "-----------------------------------------"
-            self.init_state = info[0]
-            self.init_state_d = self.on_gpu(info[0])
-            print info[0], info[1], info[2]
+            info = self.plotter(U_old.reshape(T,4), self.state,1,trees = [1,1,10,.25,2,4,10,.25, 4,1,10,.25,5,3,10,.25, 3,4,10,.25], plot=False)
+            #print p_d_test
+            #print "-----------------------------------------"
+            self.state= info[0]
+            self.state_d = self.on_gpu(info[0])
+            #print info[0], info[1], info[2]
             X.extend(info[1])
             Y.extend(info[2])
             Z.extend(info[3])
@@ -291,7 +286,7 @@ class M_Pi_2:
         X = np.array(X)
         Y = np.array(Y)
         Z = np.array(Z)
-        self.draw_tree(ax, (2,1,10,.25))
+        self.draw_tree(ax, (1,1,10,.25))
         self.draw_tree(ax, (2,4,10,.25))
         self.draw_tree(ax, (4,1,10,.25))
         self.draw_tree(ax, (5,3,10,.25))
@@ -305,8 +300,8 @@ class M_Pi_2:
         x,y,h,r = tree_info
         U = np.arange(0,2*np.pi, .05)
         length = U.size
-        print length
-        print h
+        #print length
+        #print h
         V = np.arange(0,h,h/(1.0*length))
         print V.size
         X = np.zeros((length,length))
@@ -318,7 +313,7 @@ class M_Pi_2:
             Z[:,i] = V[i]
         ax.plot_surface(X,Y,Z)
         
-    def plotter(self, U, init_state, jumps, trees = [], plot = False):
+    def plotter(self, U, state, jumps, trees = [], plot = False):
         dt = self.dt
         T = self.T
         #fig = plt.figure()
@@ -326,7 +321,7 @@ class M_Pi_2:
         X = []
         Y = []
         Z = []
-        s = init_state
+        s = state
         hover_speed = 3167.77
         w = np.zeros(4)
         max_speed = 9000
@@ -335,8 +330,10 @@ class M_Pi_2:
         for t in range(jumps):
             for i in range(len(trees)/4):
                 if np.sqrt((s[0] - trees[4*i + 0])**2 + (s[1] - trees[4*i + 1])**2) < trees[4*i + 3] and trees[4*i + 2] > s[2]:     
-                    for k in range(100):
+                    for k in range(1):
                         print "HIT A TREE!"
+                        print trees[4*i:4*i+3]
+                        print s
                     break
             w[0] = hover_speed + U[t,0] - U[t,2] + U[t,3];
             w[1] = hover_speed + U[t,0] + U[t,1] - U[t,3];
@@ -445,7 +442,6 @@ class M_Pi_2:
     #define K %d
     
     __device__ __constant__ float U_d[T*CONTROL_DIM];
-    __device__ __constant__ float p[16];
     
     __device__ int get_crash(float* s, float* t, int num_trees) 
     {
@@ -462,22 +458,17 @@ class M_Pi_2:
        return crash;
     }
 
-    __device__ float get_state_cost(float* s, float* u, float* trees, int mode, int num_trees)
+    __device__ float get_state_cost(float* s, float* u, float* p, float* trees, int num_trees)
     { 
        float cost = 0;
        float ground_penalty = 0;
        float tilt_penalty = 0;
        float crash_penalty = 0;
-       if (mode == 1) {
-          cost =  .1*( (s[0]-p[0])*(s[0]-p[0]) + (s[1]-p[1])*(s[1]-p[1]) + (s[2] - p[2])*(s[2] - p[2]) );
-       }
-       if (mode == 2) {
-          cost = 1*( (u[0]/1000.0)*(u[0]/1000.0) + (u[1]*u[1]) + (u[2]*u[2])+(u[3]*u[3]) )
-                 + 1*( (s[3]*s[3]) + (s[4]*s[4]) + (s[5]*s[5]) ) 
-                 + 5*( (s[0]-p[0])*(s[0]-p[0]) + (s[1]-p[1])*(s[1]-p[1]) + (s[2]-p[2])*(s[2]-p[2]) )
-                 + 1*( (s[6]*s[6]) + (s[7]*s[7]) + (s[8]*s[8]) )
-                 + 1*( (s[9]*s[9]) + (s[10]*s[10]) + (s[11]*s[11]) );
-       }
+       cost = 1*( (u[0]/1000.0)*(u[0]/1000.0) + (u[1]*u[1]) + (u[2]*u[2])+(u[3]*u[3]) )
+              + 1*( (s[3]*s[3]) + (s[4]*s[4]) + (s[5]*s[5]) ) 
+              + 5*( (s[0]-p[0])*(s[0]-p[0]) + (s[1]-p[1])*(s[1]-p[1]) + (s[2]-p[2])*(s[2]-p[2]) )
+              + 1*( (s[6]*s[6]) + (s[7]*s[7]) + (s[8]*s[8]) )
+              + 1*( (s[9]*s[9]) + (s[10]*s[10]) + (s[11]*s[11]) );
        if (s[2]*s[2] < .1 && s[8] < 0) {
           ground_penalty = 100;
        }
@@ -492,18 +483,9 @@ class M_Pi_2:
        return cost;
     }
 
-    __device__ float get_terminal_cost(float* s, int mode)
+    __device__ float get_terminal_cost(float* s, float* p)
     {
        float cost = 0;
-       if (threadIdx.x == 0 && blockIdx.x == 0 && mode == 1) {
-          printf("%%f,  %%f,  %%f ||", p[0],p[1],p[2]);
-       }
-       if (mode == 1) {
-          cost = 0*( (s[0]-p[0])*(s[0]-p[0]) + (s[1] - p[1])*(s[1] - p[1]) + (s[2]-p[2])*(s[2]-p[2]) );
-       }
-       if (mode == 2) {
-          cost = 0;
-       }
        return cost;
     }
    
@@ -515,11 +497,11 @@ class M_Pi_2:
     *******************************************/
    
     __global__ void rollout_kernel(float* controls_d, float* state_costs_d, float* terminal_costs_d,
-                                   float* du, float* init_state, float* trees, float dt, float motor_gain, 
+                                   float* du, float* init_state, float* trees, float* goal_state, 
+                                   float dt, float motor_gain, 
                                    float mass, float Ix, float Iy, float Iz, float l, 
-                                   float var1, float var2, int* mode1)
+                                   float var1, float var2)
     {
-       int mode = mode1[0];
        int num_trees = 5;
        //Get thread and block index
        int tdx = threadIdx.x;
@@ -533,6 +515,7 @@ class M_Pi_2:
        float min_tilt = -4000;
        //Initialize local state
        float s[STATE_DIM];
+       float p[STATE_DIM];
        float u[CONTROL_DIM];
        float w[CONTROL_DIM];
        float hover_speed = 3167.77;
@@ -542,14 +525,17 @@ class M_Pi_2:
        for (i = 0; i < STATE_DIM; i++) {
           s[i] = init_state[i];
        }
-       if (64*bdx + tdx < K) {
+       for (i = 0; i < STATE_DIM; i++) {
+          p[i] = goal_state[i];
+       }
+       if (blockDim.x*bdx + tdx < K) {
           for (i = 0; i < T; i++) {
              for (j = 0; j < CONTROL_DIM; j++) {
                 if (j %% 4 == 0) {
-                   u[j] = U_d[i*CONTROL_DIM + j] + du[(64*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j]*var1;
+                   u[j] = U_d[i*CONTROL_DIM + j] + du[(blockDim.x*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j]*var1;
                 }
                 else {
-                    u[j] = U_d[i*CONTROL_DIM + j] + du[(64*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j]*var2;
+                    u[j] = U_d[i*CONTROL_DIM + j] + du[(blockDim.x*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j]*var2;
                 }
              }
              if (u[0] > max_thrust) {
@@ -639,18 +625,18 @@ class M_Pi_2:
                 s[15] += dt*motor_gain*(w[3] - s[15]);
              }
              //Get State Costs
-             float cost = get_state_cost(s, u, trees, mode, num_trees);
+             float cost = get_state_cost(s, u, p, trees, num_trees);
              //Add costs into state_costs 
-             state_costs_d[(64*bdx + tdx)*T + i] = cost;
+             state_costs_d[(blockDim.x*bdx + tdx)*T + i] = cost;
              //Add controls
              for (j = 0; j < CONTROL_DIM; j++) {
-                controls_d[(64*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j] = u[j];
+                controls_d[(blockDim.x*bdx + tdx)*T*CONTROL_DIM + i*CONTROL_DIM + j] = u[j];
              }
           }
           //Get Terminal Costs
-          if (64*bdx + tdx < K) {
-              float terminal_cost = get_terminal_cost(s, mode);
-              terminal_costs_d[64*bdx + tdx] = terminal_cost;
+          if (blockDim.x*bdx + tdx < K) {
+              float terminal_cost = get_terminal_cost(s, p);
+              terminal_costs_d[blockDim.x*bdx + tdx] = terminal_cost;
           }
        }
     }
@@ -658,8 +644,7 @@ class M_Pi_2:
         mod = SourceModule(template)
         func = mod.get_function("rollout_kernel")
         U_d = mod.get_global("U_d")[0]
-        p_d = mod.get_global("p")[0]
-        return func, U_d, p_d
+        return func, U_d
 
     
     def func2(self, T):
@@ -770,9 +755,9 @@ class M_Pi_2:
         return a_d
 
 if __name__ == "__main__":
-    init_state = np.zeros(16, dtype=np.float32)
-    init_state[12:] = 3167.77
-    init_state[2] = 0
+    state= np.zeros(16, dtype=np.float32)
+    state[12:] = 3167.77
+    state[2] = 0
     T = 150
     K = 1000
     U = np.zeros(T*4, dtype = np.float32)
@@ -780,10 +765,10 @@ if __name__ == "__main__":
     var2 = 1.5
     var11 = var1
     var22 = var2
-    forest = np.array([2,1,10,.25,2,4,10,.25, 4,1,10,.25,5,3,10,.25, 3,4,10,.25], dtype = np.float32)
+    forest = np.array([1,1,10,.25,2,4,10,.25, 4,1,10,.25,5,3,10,.25, 3,4,10,.25], dtype = np.float32)
     p = np.array([0,0,1.5])
     #print p
-    A = M_Pi_2(init_state, forest, K, 3, T)
+    A = M_Pi_2(state, forest, K, 3, T)
     p_d = A.on_gpu(p)
     U = A.on_gpu(U)
   
@@ -809,6 +794,6 @@ if __name__ == "__main__":
         count += 1
     #print time.clock() - start
     print count
-    init_state = A.plotter(U.get().reshape(T,4), init_state, jumps, plot = True)
+    state= A.plotter(U.get().reshape(T,4), state, jumps, plot = True)
     """
     A.multi_pi()
